6177633c44239039ef8284ac0053416a92759463
MisaOgura <misa.ogura01@gmail.com>
2018-03-01 19:20:09 +0000

    Refactor some function names

--- src/client/evaluate.js
+++ src/client/evaluate.js
@@ -3,9 +3,9 @@ export function shouldLive (grid, coord) {
   const y = coord[1]
   const isCellAlive = grid[x][y] === 1
 
-  const neighbouringGrid = calculateNeighbouringGrid(grid, x, y)
+  const neighbouringGrid = cropNeighbouringGrid(grid, x, y)
 
-  const totalCellsAlive = neighbouringGrid.map(filterLiveCells).reduce(flattenArray, 0)
+  const totalCellsAlive = neighbouringGrid.map(filterLiveCells).reduce(sumOfCells, 0)
   const neighbours = isCellAlive ? totalCellsAlive - 1 : totalCellsAlive
 
   const underPopulated = neighbours < 2
@@ -18,28 +18,28 @@ function filterLiveCells (row) {
   return row.filter(cell => cell === 1)
 }
 
-function flattenArray (accumulator, currentValue) {
+function sumOfCells (accumulator, currentValue) {
   return accumulator + currentValue.length
 }
 
-function calculateNeighbouringGrid (grid, x, y) {
+function cropNeighbouringGrid (grid, x, y) {
   const gridX = grid.length - 1
   const gridY = grid[0].length - 1
 
-  const beginRow = foo(x)
-  const endRow = bar(x, gridX)
+  const beginRow = minIndexAlong(x)
+  const endRow = maxIndexAlong(x, gridX)
 
-  const beginColumn = foo(y)
-  const endColumn = bar(y, gridY)
+  const beginColumn = minIndexAlong(y)
+  const endColumn = maxIndexAlong(y, gridY)
 
   return grid.slice(beginRow, endRow).map(row => row.slice(beginColumn, endColumn))
 }
 
-function foo (loc) {
+function minIndexAlong (loc) {
   return loc - 1 < 0 ? loc : loc - 1
 }
 
-function bar (loc, max) {
+function maxIndexAlong (loc, max) {
   return loc + 1 > max ? loc + 1 : loc + 2
 }
 
