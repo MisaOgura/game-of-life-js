1e90bfea050ef8abb18ac72f48631215256baa14
MisaOgura <misa.ogura01@gmail.com>
2018-03-01 17:21:02 +0000

    Refactor logic to count total live cells

--- src/client/evaluate.js
+++ src/client/evaluate.js
@@ -1,12 +1,13 @@
 export function shouldLive (grid, coord) {
-  const isCellAlive = grid[coord[0]][coord[1]] === 1
+  const x = coord[0]
+  const y = coord[1]
 
-  let numOfCellsAlive = 0
-  grid.forEach(row => {
-    numOfCellsAlive += row.filter(cell => cell === 1).length
-  })
+  const isCellAlive = grid[x][y] === 1
 
-  const neighbours = isCellAlive ? numOfCellsAlive - 1 : numOfCellsAlive
+  const totalCellsAlive = grid
+    .map(filterLiveCells).reduce(flattenArray).length
+
+  const neighbours = isCellAlive ? totalCellsAlive - 1 : totalCellsAlive
 
   const underPopulated = neighbours < 2
   const overPopulated = neighbours > 3
