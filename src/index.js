38a898369b70426ec191efd491e8f2410fbdec4e
MisaOgura <misa.ogura01@gmail.com>
2018-06-21 12:17:58 +0100

    Refactor dup code

--- src/index.js
+++ src/index.js
@@ -8,31 +8,19 @@ let imageData = ctx.getImageData(0, 0, WIDTH, HEIGHT)
 
 console.time('initialisation')
 const gameOfLife = new GameOfLife(WIDTH, HEIGHT)
-// gameOfLife.populate(INITPOPULATION)
 console.log(`num of initial population: ${gameOfLife.numOfLiveCells}`)
 console.timeEnd('initialisation')
 
-// flatten grid
-const flattenedGrid = gameOfLife.grid.reduce((accum, currentValue) => accum.concat(currentValue))
-
-const data = []
-flattenedGrid.map(cell => {
-  const isCellAlive = cell === ALIVE
-  isCellAlive ? data.push(0, 0, 0, 255) : data.push(0, 0, 0, 0)
-})
-
-for (let i = 0; i < imageData.data.length; i += 4) {
-  imageData.data[i] = data[i]
-  imageData.data[i + 1] = data[i + 1]
-  imageData.data[i + 2] = data[i + 2]
-  imageData.data[i + 3] = data[i + 3]
-}
-
-ctx.putImageData(imageData, 0, 0)
+updateGeneration(gameOfLife, imageData)
 
 function evolve () {
   gameOfLife.evolve()
+  updateGeneration(gameOfLife, imageData)
+}
 
+setInterval(evolve, SPEED)
+
+function updateGeneration (gameOfLife, imageData) {
   const flattenedGrid = gameOfLife.grid.reduce((accum, currentValue) => accum.concat(currentValue))
 
   const data = []
@@ -50,5 +38,3 @@ function evolve () {
 
   ctx.putImageData(imageData, 0, 0)
 }
-
-setInterval(evolve, SPEED)
